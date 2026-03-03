6cf57c70b06215270f64730cbd49b0be7acad7ec
BhatRajendra <rajendrabhat0408@gmai.com>
2026-03-03 11:09:08 +0530

    update nvim sciprt to install from tar instead of using apt

--- install/nvim.sh
+++ install/nvim.sh
@@ -4,8 +4,20 @@ set -euo pipefail
 if command -v nvim &> /dev/null; then
     echo "Neovim already installed: $(nvim --version | head -n1)"
 else
-    echo "Installing Neovim..."
-    sudo apt update
-    sudo apt install -y neovim
-    echo "Neovim installed."
+    echo "Installing latest Neovim..."
+
+    sudo apt install -y curl
+
+    cd /tmp
+    curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+
+    sudo rm -rf /opt/nvim-linux-x86_64
+    sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
+    sudo ln -sf /opt/nvim-linux-x86_64/bin/nvim /usr/local/bin/nvim
+
+    rm nvim-linux-x86_64.tar.gz
+
+    echo "Neovim installed successfully."
 fi
+
